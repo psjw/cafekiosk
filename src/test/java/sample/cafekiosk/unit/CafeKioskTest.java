705689b8da9e322f6c06c5466d15b2b895faa5bf
psjw <dlrudtn108@gmail.com>
2024-06-09 23:47:17 +0900

    firstcommit

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -26,7 +26,8 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
-    @DisplayName("음료 1개 추가 테스트")
+//    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -126,17 +127,19 @@ class CafeKioskTest {
                 .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice(){
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
 
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
-
+        //when
         int totalPrice = cafeKiosk.calculateTotalPrice();
-
+        //then
         assertThat(totalPrice).isEqualTo(8500);
     }
 }
