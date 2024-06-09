93e4673b8164caa39a37c5f2dc0572f45855207a
psjw <dlrudtn108@gmail.com>
2024-06-09 20:38:49 +0900

    first commit

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -23,10 +23,10 @@ public class CafeKiosk {
     }
 
     public void add(Beverage beverage, int count) {
-        if(count <= 0){
+        if (count <= 0) {
             throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
         }
-        for(int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             beverages.add(beverage);
         }
     }
@@ -35,21 +35,28 @@ public class CafeKiosk {
         beverages.remove(beverage);
     }
 
-    public void  clear(){
+    public void clear() {
         beverages.clear();
     }
 
+//    public int calculateTotalPrice() {
+//        int totalPrice = 0;
+//
+//        for (Beverage beverage : beverages) {
+//            totalPrice += beverage.getPrice();
+//        }
+//
+//        return totalPrice;
+//    }
+
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();
-        }
-
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
-    public Order createOrder(){
+
+    public Order createOrder() {
         LocalDateTime currentDateTime = LocalDateTime.now();
         LocalTime currentTime = currentDateTime.toLocalTime();
         if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
@@ -59,7 +66,7 @@ public class CafeKiosk {
         return new Order(currentDateTime, beverages);
     }
 
-    public Order createOrder(LocalDateTime currentDateTime){
+    public Order createOrder(LocalDateTime currentDateTime) {
 //        LocalDateTime currentDateTime = LocalDateTime.now();
         LocalTime currentTime = currentDateTime.toLocalTime();
         if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
@@ -68,4 +75,5 @@ public class CafeKiosk {
 
         return new Order(currentDateTime, beverages);
     }
+
 }
